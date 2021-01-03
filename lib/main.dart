a96d2cae62c88fc25147efbdc2df522086ce86c4
pvphero <impvphero@163.com>
2021-01-03 23:13:15 +0800

    frist

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:wanandroid_flutter_app/util/favoriteProvide.dart';
+import 'package:wanandroid_flutter_app/util/themeProvide.dart';
 
 void main() async {
   //runApp前调用，初始化 绑定 手势 渲染 服务等
@@ -6,4 +8,5 @@ void main() async {
 
   //初始化
   var theme = ThemeProvide();
+  var favorite = FavoriteProvide();
 }
