26910386b4c087a4d7234ce23994932078d76800
pvphero <impvphero@163.com>
2021-01-03 23:05:06 +0800

    frist

--- lib/main.dart
+++ lib/main.dart
@@ -3,4 +3,7 @@ import 'package:flutter/material.dart';
 void main() async {
   //runApp前调用，初始化 绑定 手势 渲染 服务等
   WidgetsFlutterBinding.ensureInitialized();
+
+  //初始化
+  var theme = ThemeProvide();
 }
