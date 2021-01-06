feaa1e2c17d303c99469d9fe3da7404456f4f8a7
pvphero <impvphero@163.com>
2021-01-06 17:15:01 +0800

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:wanandroid_flutter_app/util/favoriteProvide.dart';
-import 'package:wanandroid_flutter_app/util/themeProvide.dart';
+import 'package:provide/provide.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:wanandroid_flutter/pages/homePage.dart';
+import 'package:wanandroid_flutter/res/colors.dart';
+import 'package:wanandroid_flutter/res/strings.dart';
+import 'package:wanandroid_flutter/util/favoriteProvide.dart';
+import 'package:wanandroid_flutter/util/themeProvide.dart';
 
 void main() async {
   //runApp前调用，初始化 绑定 手势 渲染 服务等
@@ -9,4 +14,88 @@ void main() async {
   //初始化
   var theme = ThemeProvide();
   var favorite = FavoriteProvide();
+  var providers = Providers();
+
+  //.. 相当于  Providers providers= Providers();
+  //          providers.provide(...);
+  //          providers.provide(...);
+  providers
+    ..provide(Provider.function((context) => theme))
+    ..provide(Provider.function((context) => favorite));
+
+  int themeIndex = await getTheme();
+
+  runApp(ProviderNode(
+    child: MyApp(themeIndex),
+    providers: providers,
+  ));
+}
+
+Future<int> getTheme() async {
+  SharedPreferences sp = await SharedPreferences.getInstance();
+  int themeIndex = sp.getInt("themeIndex");
+  return null == themeIndex ? 0 : themeIndex;
+}
+
+/*
+快捷键  stl
+ */
+class MyApp extends StatelessWidget {
+  final int themeIndex;
+
+  MyApp(this.themeIndex);
+
+  @override
+  Widget build(BuildContext context) {
+    return Provide<ThemeProvide>(builder: (context, child, theme) {
+      return MaterialApp(
+        title: YString.appName,
+        //除了primaryColor，还有brightness，iconTheme，textTheme等等可以设置
+        theme: ThemeData(
+            primaryColor: YColors
+                    .themeColor[theme.value != null ? theme.value : themeIndex]
+                ["primaryColor"],
+            primaryColorDark: YColors
+                    .themeColor[theme.value != null ? theme.value : themeIndex]
+                ["primaryColorDark"],
+            accentColor: YColors
+                    .themeColor[theme.value != null ? theme.value : themeIndex]
+                ["colorAccent"]
+//              primaryColor: YColors.colorPrimary,
+//              primaryColorDark: YColors.colorPrimaryDark,
+//              accentColor: YColors.colorAccent,
+//              dividerColor: YColors.dividerColor,
+            ),
+        home: MyHomePage(title: YString.appName),
+      );
+    });
+  }
+}
+
+/**
+ * 快捷键 stful
+ */
+class MyHomePage extends StatefulWidget {
+  final String title;
+
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+  String title = YString.appName;
+
+  var _pageController = PageController(initialPage: 0);
+
+  var page = <Widget>[
+    HomePage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
---
+++ lib/res/strings.dart
@@ -0,0 +1,10 @@
+class YString {
+  static String appName = "玩安卓";
+  static String proName = "wandroid_flutter";
+  static String github = "https://github.com/pvphero/wanandroid_flutter_app";
+
+  static String home = "首页";
+  static String tree = "体系";
+  static String navi = "导航";
+  static String project = "项目";
+}
