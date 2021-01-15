5507f35028a7c090b988aad684b06bab71bacc58
pvphero <impvphero@163.com>
2021-01-15 14:09:52 +0800

    home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:provide/provide.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:wanandroid_flutter/pages/homePage.dart';
@@ -90,12 +91,105 @@ class _MyHomePageState extends State<MyHomePage> {
 
   var _pageController = PageController(initialPage: 0);
 
-  var page = <Widget>[
+  var pages = <Widget>[
     HomePage(),
   ];
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.search),
+              tooltip: '搜索',
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => HomePage()));
+              })
+        ],
+      ),
+      body: PageView.builder(
+          onPageChanged: _pageChage,
+          controller: _pageController,
+          itemCount: pages.length,
+          itemBuilder: (BuildContext context, int index) {
+            return pages.elementAt(_selectedIndex);
+          }),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text(YString.home),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.filter_list),
+            title: Text(YString.tree),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.low_priority),
+            title: Text(YString.navi),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.apps),
+            title: Text(YString.project),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        type: BottomNavigationBarType.fixed,
+        fixedColor: Theme.of(context).primaryColor,
+        onTap: _onItemTapped,
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Theme.of(context).primaryColor,
+        onPressed: showToast,
+        tooltip: '点击选中最后一个',
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
+      drawer: showDrawer(),
+    );
   }
+
+  void _pageChage(int index) {
+    setState(() {
+      _selectedIndex = index;
+      switch (index) {
+        case 0:
+          title = YString.appName;
+          break;
+        case 1:
+          title = YString.tree;
+          break;
+        case 2:
+          title = YString.navi;
+          break;
+        case 3:
+          title = YString.project;
+          break;
+      }
+    });
+  }
+
+  void _onItemTapped(int index) {
+    _pageController.animateToPage(index,
+        duration: Duration(milliseconds: 300), curve: Curves.ease);
+  }
+
+  void showToast() {
+    Fluttertoast.showToast(
+        msg: "选中最后一个",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIos: 1,
+        backgroundColor: YColors.colorPrimary,
+        textColor: Colors.white,
+        fontSize: 16.0);
+    _onItemTapped(3);
+  }
+
+  Widget showDrawer() {}
 }
--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:core';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_easyrefresh/easy_refresh.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:wanandroid_flutter/common/api.dart';
 import 'package:wanandroid_flutter/entity/article_entity.dart';
@@ -42,7 +43,7 @@ class _HomePageState extends State<HomePage> {
 
       //article
       var articleResponse =
-          await HttpUtil().get(Api.ARTICLE_LIST + "$_page/json");
+      await HttpUtil().get(Api.ARTICLE_LIST + "$_page/json");
       Map articlemap = json.decode(articleResponse.toString());
       var articleEntity = ArticleEntity().fromJson(articlemap);
 
@@ -58,6 +59,54 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: EasyRefresh.custom(
+        header: PhoenixHeader(),
+        footer: PhoenixFooter(),
+        onRefresh: () async {
+          await Future.delayed(Duration(
+            seconds: 1,
+          ),
+                  () {
+                setState(() {
+                  _page = 0;
+                });
+                getHttp();
+              });
+        },
+
+        onLoad: () async {
+          await Future.delayed(
+              Duration(
+                seconds: 1,
+              ),
+                  () async {
+                setState(() {
+                  _page++;
+                });
+                getMoreData();
+              }
+          );
+        },
+
+      ),
+    );
+  }
+
+  void getMoreData() async {
+    var response = await HttpUtil().get(Api.ARTICLE_LIST + "$_page/josn");
+    Map map = json.decode(response.toString());
+    var articleEntity = ArticleEntity().fromJson(map);
+    setState(() {
+      articleDates.addAll(articleEntity.data.datas);
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    _swiperController.stopAutoplay();
+    _swiperController.dispose();
+    super.dispose();
   }
 }
